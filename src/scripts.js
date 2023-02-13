33b07d7f97e67347a8172452920adbb8a330fa9d
Sophie LaBelle <sophie.r.labelle@gmail.com>
2023-02-12 20:07:41 -0800

    Update single recipe view with styling elements

--- src/scripts.js
+++ src/scripts.js
@@ -5,54 +5,40 @@ import Recipe from '../src/classes/Recipe';
 import Ingredient from './classes/Ingredient';
 import User from './classes/User';
 
-
-
 Promise.all([fetchData('users'), fetchData('ingredients'), fetchData('recipes')])
 .then(vals => {
   let userData = vals[0].usersData;
   let ingredientsData = vals[1].ingredientsData;
   let recipesData = vals[2].recipeData;
   let recipeRepo = new RecipeRepository(recipesData, ingredientsData);
-  // use the make recipe list method on recipeRepo var to get the recipe list as isntances of Recipe
   insertRecipeCards(recipesData);
-  getRandomUser(userData)
+  getRandomUser(userData);
   cardTileDisplay.addEventListener('click', (event) => {
     if (event.target.closest('.card') || event.target.id === event) {
       showSingleRecipe(event, recipeRepo, ingredientsData);
-    }
+    };
   });
-})
-
-
-
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
-// import './images/turing-logo.png'
+});
 
 const searchBarBtn = document.querySelector('#searchBtn');
 const savedViewBtn = document.querySelector('#savedViewBtn');
 const homeViewBtn = document.querySelector('#homeViewBtn');
 const infoBtn = document.querySelector('#infoBtn');
-const searchBarInput =document.querySelector('#searchBar');
 
 const cardTileDisplay = document.querySelector('#cardTileView');
 const singleRecipeDisplay = document.querySelector('#singleRecipeView');
 const insertUserName = document.querySelector('#userName');
+const searchBarInput =document.querySelector('#searchBar');
 const nameSearchResults = document.querySelector('#nameResultsView');
 const tagSearchResults = document.querySelector('#tagResultsView');
 
-const creatorDisplay = document.querySelector('#creatorInfoPage');
-// const recipeRepository = new RecipeRepository(recipeData);
+const creatorDisplay = document.querySelector;
 
 
 // Event Listeners
-// window.addEventListener('load', () => {
-//   insertRecipeCards(recipesData)
-//   getRandomUser();
-// });
-
 homeViewBtn.addEventListener('click', () => {
   showHomeView();
-})
+});
 
 searchBarBtn.addEventListener( 'click', function() {
   cardTileDisplay.innerHTML = "";
@@ -66,7 +52,7 @@ searchBarInput.addEventListener('change', () => {
   getRecipeByTag();
   getRecipeByName();
   displayNoResults();
-})
+});
 
 infoBtn.addEventListener('click', showInfo);
 
@@ -79,7 +65,7 @@ function getRecipeByTag() {
     tagSearchResults.innerHTML += `<section class="tagResults"><h1 class="searched-recipe" id=${result.tags}></h1></section>`
   });
   insertRecipeCards(tagResults);
-}
+};
 
 function getRecipeByName() {
   let nameResults = [];
@@ -104,19 +90,13 @@ function welcomeUser(user) {
   };
 };
 
-// function displayNoResults() {
-//   if (nameResults.length === 0 && tagResults.length === 0) {
-//     nameSearchResults.innerHTML = `<h1>No results found.</h1>`
-//   }
-// }
-
 function insertRecipeCards(array) {
   for(let i = 0; i < array.length; i++){
     cardTileDisplay.innerHTML += 
       `<section class="card" id="${array[i].id}">
       <h2 id="${array[i].id}">${array[i].name}</h2>
       <img src="${array[i].image}" alt="image of ${array[i].name} id="${array[i].id}">
-      </section>`;
+      </section>`
   };
 };
 
@@ -126,13 +106,10 @@ function showSingleRecipe(event, repo, ingredients) {
   hide(cardTileDisplay);
   let fetchedIng = ingredients;
   const element = event.target.id
-  console.log('event.target.id:', element)
   const foundRecipe = repo.findRecipe(element)
-  console.log('found recipe before retrieval:', foundRecipe)
   foundRecipe.todosIngredients = fetchedIng;
   let foundIngredients = foundRecipe.retrieveIngredientInfo();
   let foundInstructions = foundRecipe.giveInstructionsForRecipe();
-  // console.log('return cost?', foundRecipe.returnCostOfIngredients())
   singleRecipeDisplay.innerHTML = 
   `<section class="single-recipe" id="${foundRecipe.id}">
   <h2>${foundRecipe.name}</h2>
@@ -144,14 +121,16 @@ function showSingleRecipe(event, repo, ingredients) {
     <input type="radio" name="rating" value="2" id="2"><label for="2">☆</label>
     <input type="radio" name="rating" value="1" id="1"><label for="1">☆</label>
   </div>
-  <div>
+  <h3>Ingredients</h3>
+  <div class="ingredients">
     <p>${foundIngredients.map((ing) => ` ${ing.name}`)}</p>
   </div>
-  <div>
+  <h3>Instructions</h3>
+  <div class="instructions">
     <p>${foundInstructions.map((inst) =>` ${inst}`)}</p>
   </div>
   </section>`
-}
+};
 
 // Functions
 function showHomeView() {
@@ -159,13 +138,13 @@ function showHomeView() {
   hide(singleRecipeDisplay);
   hide(homeViewBtn);
   hide(creatorDisplay);
-}
+};
 
 function showInfo() {
   show(creatorDisplay);
   show(homeViewBtn);
   hide(cardTileDisplay);
-}
+};
 
 // Helper Functions
 function show(element) {
@@ -174,8 +153,4 @@ function show(element) {
   
 function hide(element) {
   element.classList.add('hidden');
-};
-
-function getRandomIndex(array) {
-  return Math.floor(Math.random() * array.length);
 };
